10c9901cc9f373d38645d35d30fbeba9b0fa79ed
AMP26 <amit.patnaik0126@gmail.com>
2025-09-22 13:51:20 +0530

    Java Basics

--- 01-Basics/src/datatypes/Datatype.java
+++ 01-Basics/src/datatypes/Datatype.java
@@ -1,4 +1,20 @@
 package datatypes;
 
 public class Datatype {
+    // Integral Datatypes
+    public byte a = 10; // Range: -128 to 127 [-2e7 to 2e7-1]
+    public short b = 20; // Range: -32768 to 32767 [-2e15 to 2e15-1]
+    public int c = 30; // Range: -2147483648 to 2147483647 [-2e31 to 2e31-1]
+    public long d = 40; // Range: -2e63 to 2e63-1
+
+    // Floating-point Datatypes
+    public float e = 50.3f; // Range: -3.4e38 to 3.4e38.
+    public double f = 60.4; // Range: -1.7e308 to1.7e308
+
+    // Boolean Datatype
+    public boolean g = true;
+    public boolean h = false;
+
+    // Character Datatype
+    char i = 'a'; // Range: 0 to 65535
 }
--- 01-Basics/src/datatypes/Main.java
+++ 01-Basics/src/datatypes/Main.java
@@ -1,4 +1,20 @@
 package datatypes;
 
 public class Main {
+    public static void main(String[] args) {
+        Datatype dt = new Datatype();
+
+        System.out.println("byte: " + dt.a);
+        System.out.println("short: " + dt.b);
+        System.out.println("int: " + dt.c);
+        System.out.println("long: " + dt.d);
+
+        System.out.println("float: " + dt.e);
+        System.out.println("double: " + dt.f);
+
+        System.out.println("boolean: " + dt.g);
+        System.out.println("boolean: " + dt.h);
+
+        System.out.println("char: " + dt.i);
+    }
 }
