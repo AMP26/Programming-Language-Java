a8e2394e1e5fd1eb4777ab42bb589615f03571a1
AMP26 <amit.patnaik0126@gmail.com>
2025-09-24 11:39:28 +0530

    Java Basics - Operators

--- 01-Basics/src/operators/Main.java
+++ 01-Basics/src/operators/Main.java
@@ -2,6 +2,11 @@ package operators;
 
 public class Main {
     public static void main(String[] args) {
+        Operators ops = new Operators();
 
+        ops.arithmeticOperators();
+        ops.relationalOperators();
+        ops.logicalOperators();
+        ops.bitwiseOperators();
     }
 }
