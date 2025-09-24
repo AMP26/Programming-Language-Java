360ce57f23412ac73d903a4d170993d58fca04a0
AMP26 <amit.patnaik0126@gmail.com>
2025-09-24 11:37:02 +0530

    Java Basics - Operators

--- 01-Basics/src/operators/Operators.java
+++ 01-Basics/src/operators/Operators.java
@@ -22,4 +22,12 @@ public class Operators {
         System.out.println("a >= b: " + (a >= b));
         System.out.println("a <= b: " + (a <= b));
     }
+
+    public void logicalOperators() {
+        boolean x = true, y = false;
+        System.out.println("\nLogical Operators:");
+        System.out.println("x && y: " + (x && y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("!x: " + (!x));
+    }
 }
