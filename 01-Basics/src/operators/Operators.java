81a016df4751aba883e3861cb0f0f994e73fc33c
AMP26 <amit.patnaik0126@gmail.com>
2025-09-24 11:38:41 +0530

    Java Basics - Operators

--- 01-Basics/src/operators/Operators.java
+++ 01-Basics/src/operators/Operators.java
@@ -30,4 +30,26 @@ public class Operators {
         System.out.println("x || y: " + (x || y));
         System.out.println("!x: " + (!x));
     }
+
+    public void bitwiseOperators() {
+        int a = 60; // 0011 1100
+        int b = 13; // 0000 1101
+        int result;
+
+        System.out.println("\nBitwise Operators:");
+        result = a & b;
+        System.out.println("a & b = " + result);  // 12
+        result = a | b;
+        System.out.println("a | b = " + result);  // 61
+        result = a ^ b;
+        System.out.println("a ^ b = " + result);  // 49
+        result = ~a;
+        System.out.println("~a = " + result);     // -61
+        result = a << 2;
+        System.out.println("a << 2 = " + result); // 240
+        result = a >> 2;
+        System.out.println("a >> 2 = " + result); // 15
+        result = a >>> 2;
+        System.out.println("a >>> 2 = " + result); // 15
+        }
 }
