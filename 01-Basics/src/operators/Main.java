61308bf2ff2f0097ce666940a61669885129145a
AMP26 <amit.patnaik0126@gmail.com>
2025-09-24 11:35:06 +0530

    Java Basics - Operators

--- 01-Basics/src/operators/Main.java
+++ 01-Basics/src/operators/Main.java
@@ -2,6 +2,6 @@ package operators;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
--- 01-Basics/src/operators/Operators.java
+++ 01-Basics/src/operators/Operators.java
@@ -1,4 +1,16 @@
 package operators;
 
 public class Operators {
+
+    public void arithmeticOperators() {
+        int a = 11, b = 5;
+        System.out.println("Arithmetic Operators:");
+        System.out.println("Addition: a + b = " + (a + b));
+        System.out.println("Subtraction: a - b = " + (a - b));
+        System.out.println("Multiplication: a * b = " + (a * b));
+        System.out.println("Division: a / b = " + (a / b));
+        System.out.println("Modulus: a % b = " + (a % b));
+    }
+
+    
 }
