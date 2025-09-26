a4dc0d464dd1ecc4450f883fdfd34400b221da98
AMP26 <amit.patnaik0126@gmail.com>
2025-09-26 10:56:38 +0530

    String Literals

---
+++ 01-Basics/src/literals/StringLiterals.java
@@ -0,0 +1,11 @@
+package literals;
+
+public class StringLiterals {
+    public static void main(String[] args) {
+        String greeting = "Hello, World!";
+
+        System.out.println("String Literal:");
+        System.out.println(greeting);
+    }
+}
+
